c01c1c82ed584060d38b0f765983830295456e6c
BigBabyDake <47224883+danialzinolla@users.noreply.github.com>
2020-04-30 21:16:40 +0600

    Add files via upload

---
+++ lab7/src/app/categories.ts
@@ -0,0 +1,18 @@
+export const categories = [
+  {
+    id: 1,
+    name: 'Book'
+  },
+  {
+    id: 2,
+    name: 'Book2'
+  },
+  {
+    id: 3,
+    name: 'Book3'
+  },
+  {
+    id: 4,
+    name: 'Book4'
+  }
+]
---
+++ lab7/src/app/products.ts
@@ -0,0 +1,31 @@
+export const products = [
+  {
+    id: 1,
+    name: 'book',
+    description: 'book',
+    rating: 10.0,
+    category_id: 1,
+      },
+  {
+    id: 2,
+    name: 'book',
+    description: 'book',
+    rating: 8.0,
+    category_id: 2,
+      },
+  {
+    id: 3,
+    name: 'book',
+    description: 'book',
+    rating: 7.0,
+    category_id: 3,
+    },
+  {
+    id: 4,
+    name: 'book',
+    description: 'book',
+    rating: 10.0,
+    category_id: 4,
+  },
+  
+];
